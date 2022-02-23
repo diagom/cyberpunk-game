4e88686ae25d0f4df9faae030d9d93401b821f8e
diagom <d-asantana@live.com>
2022-02-22 22:53:32 -0300

    adição parcial de chão

--- canvas.js
+++ canvas.js
@@ -2,14 +2,23 @@ console.log("[dev]d");
 
 const sprites = new Image();
 const sprites2 = new Image();
+const floor = new Image();
+const floor2 = new Image();
+const floor3 = new Image();
+const floor4 = new Image();
 sprites.src = "./SPRITES/background/skyline-a.png";
 sprites2.src = "./SPRITES/background/skyline-b.png";
+floor.src = "./SPRITES/ENVIRONMENT/floor.png";
+floor2.src = "./SPRITES/ENVIRONMENT/floor2.png";
+floor3.src = "./SPRITES/ENVIRONMENT/floor3.png";
+floor4.src = "./SPRITES/ENVIRONMENT/floor4.png";
 
 const canvas = document.querySelector("canvas");
-const contexto = canvas.getContext("2d");
+const context = canvas.getContext("2d");
 
 function loop() {
-  contexto.drawImage(sprites, 0, 0, 128, 240, 0, 0, 128, 240);
+  renderBackground();
+  renderFloor();
 
   requestAnimationFrame(loop);
 }
--- variaveis.js
+++ variaveis.js
@@ -1,1 +1,24 @@
-const teste = () => {};
+let nextFloor = 40;
+const renderBackground = () => {
+  context.drawImage(sprites, 0, 0, 128, 240, 0, 0, 256, 512);
+  context.drawImage(sprites2, 0, 0, 128, 240, 256, 0, 256, 512);
+  context.drawImage(sprites, 0, 0, 128, 240, 512, 0, 256, 512);
+  context.drawImage(sprites, 0, 0, 128, 240, 768, 0, 256, 512);
+  context.drawImage(sprites, 0, 0, 128, 240, 768, 0, 256, 512);
+};
+
+const renderFloor = () => {
+  context.drawImage(floor3, 0, 0, 100, 200, 0, 375, 100, 400);
+  middleFloor();
+};
+
+const middleFloor = () => {
+  if (nextFloor < 300) {
+    context.drawImage(floor4, 0, 0, 100, 200, nextFloor, 376, 100, 400);
+    nextFloor += 30;
+  }
+};
+
+const a = {
+  largura: 30,
+};
